0f127c835db4d8310ed02c2431743b6689942259
John Wellbrock <wellbrock23john@gmail.com>
2020-12-17 23:51:52 -0700

    deployment ready

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-	API_ENDPOINT: "https://peaceful-brushlands-34809.herokuapp.com/",
+	API_ENDPOINT: "https://peaceful-brushlands-34809.herokuapp.com/api",
 	TOKEN_KEY: "process.env.REACT_APP_API_KEY",
 };
